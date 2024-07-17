46727fde5c1d2f30c8505f8c2d9fda3b7568ee22
keezo11203 <kaitlynellis0813@gmail.com>
2024-07-16 23:31:26 -0400

    first commit

---
+++ Conciertos/ConciertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConciertosApp.swift
+//  Conciertos
+//
+//  Created by Kaitlyn Ellis on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ConciertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
